8c6d02e5151d03f55b49e27ce161ab2ed0f8d1d7
Lukasz <lukasz@MacBook-Pro-Lukasz.local>
2019-01-10 08:42:15 +0100

    Convert to MVC design pattern.

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Lukasz on 10/01/2019.
+//  Copyright © 2019 Lukasz. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
